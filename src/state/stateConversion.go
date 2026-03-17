40e0d8e0d4dd8238a68162cf457b39147ad1f207
benjamhl <benjamhl@stud.ntnu.no>
2026-03-17 15:23:46 +0100

    Moved state conversion to own file

---
+++ src/state/stateConversion.go
@@ -0,0 +1,57 @@
+package state
+
+import (
+	"heislab/management"
+)
+
+// TODO: Comment
+// -------------------------------------------------------------------------------------------
+// State conversion
+// -------------------------------------------------------------------------------------------
+
+type ElevatorStateJSON struct {
+	Behavior      string `json:"behaviour"` // idle, moving, doorOpen, offline
+	Floor         int    `json:"floor"`
+	Direction     string `json:"direction"`
+	CabOrders     []bool `json:"cabRequests"`
+	CanTakeOrders bool   `json:"canTakeOrders"`
+}
+
+func ConvertElevatorToJSON(elev *management.Elevator) ElevatorStateJSON {
+	cabOrders := make([]bool, management.NumFloors)
+	for floor := 0; floor < management.NumFloors; floor++ {
+		cabOrders[floor] = elev.GetOrderActiveStatus(floor, management.CabButton)
+	}
+
+	return ElevatorStateJSON{
+		Behavior:      convertState(elev.GetState()),
+		Floor:         elev.GetLastFloor(),
+		Direction:     convertDirection(elev.GetMoveDir()),
+		CabOrders:     cabOrders,
+		CanTakeOrders: elev.GetCanTakeOrders(),
+	}
+}
+
+func convertState(state management.State) string {
+	switch state {
+	case management.ElevIdle:
+		return "idle"
+	case management.ElevMoving:
+		return "moving"
+	case management.ElevInit:
+		return "moving"
+	default:
+		return "idle"
+	}
+}
+
+func convertDirection(direction management.Direction) string {
+	switch direction {
+	case management.DirUp:
+		return "up"
+	case management.DirDown:
+		return "down"
+	default:
+		return "stop"
+	}
+}
